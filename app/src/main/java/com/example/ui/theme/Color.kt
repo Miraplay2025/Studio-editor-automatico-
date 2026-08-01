ec6ed6778340958943fbf594dc3fd7088513f551
Miraplay2025 <massingueantoniocarlos38@gmail.com>
2026-08-01 14:44:52 +0200

    Criando: app/src/main/java/com/example/ui/theme/Color.kt

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF0D0E15)
+val DarkSurface = Color(0xFF161824)
+val DarkSurfaceVariant = Color(0xFF202334)
+val PrimaryPurple = Color(0xFF7C5CFC)
+val SecondaryCyan = Color(0xFF00E5FF)
+val AccentPink = Color(0xFFFF2A85)
+val WarningRed = Color(0xFFFF3366)
+val TextPrimary = Color(0xFFF1F2F6)
+val TextSecondary = Color(0xFFA0A5C0)
+val BorderDark = Color(0xFF2A2E45)
