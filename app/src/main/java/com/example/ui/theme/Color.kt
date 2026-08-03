cd924fda1431345f74937cb1e237a6606656d968
Miraplay2025 <massingueantoniocarlos38@gmail.com>
2026-08-03 19:39:06 +0200

    Atualizando: app/src/main/java/com/example/ui/theme/Color.kt

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -2,19 +2,10 @@ package com.example.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DarkCanvas = Color(0xFF121212)
-val DarkSurface = Color(0xFF1C1B1F)
-val DarkSurfaceVariant = Color(0xFF2B2930)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
 
-val PrimaryPurple = Color(0xFFD0BCFF)
-val PrimaryPurpleText = Color(0xFF381E72)
-val PrimaryPurpleLight = Color(0xFFE8DEF8)
-val SecondaryMint = Color(0xFFCCC2DC)
-val AlertRed = Color(0xFFF2B8B5)
-
-val TextPrimary = Color(0xFFE6E1E5)
-val TextSecondary = Color(0xFFCAC4D0)
-val TextMuted = Color(0xFF938F96)
-
-val BorderLight = Color(0xFF49454F)
-val AccentGold = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
